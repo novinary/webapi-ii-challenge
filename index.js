ebd8fadbf7586ff7520b87f0c3797ca5be94e547
novinary <novina.dev@gmail.com>
2019-03-28 14:07:39 +0000

    Deploy to heroku

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const server = require('./server.js')
-// add an endpoint that returns all the messages for a hub
-// add an endpoint for adding new message to a hub
+// code away!
+// it's recommended to load configuration for .env as early as possible
+require('dotenv').config(); // add this line as the first thing to run1
 
-server.listen(5001, () => {
-  console.log('\n*** Server Running on http://localhost:5001 ***\n');
+const server = require('./server.js');
+
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const port = process.env.PORT || 4444;
+
+// we can now use that port, if set up by heroku or read from .env or 4444 as a default if not set
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
